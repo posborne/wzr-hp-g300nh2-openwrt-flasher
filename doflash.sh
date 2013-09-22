ca8b549d94b2b33487f5e21fd4196ae70da7b913
Paul Osborne <osbpau@gmail.com>
2013-09-22 16:25:43 -0500

    use tftp-hpa with options that make process less error prone

--- doflash.sh
+++ doflash.sh
@@ -9,7 +9,8 @@
 NETWORK_INTERFACE="eth0"
 
 # must be changed for G300H versus G300-NH2
-MAC_ADDRESS="02-AA-BB-CC-DD-1A"
+MAC_ADDRESS="02:AA:BB:CC:DD:1A"
+# MAC_ADDRESS="00:1B:24:34:ad:b5"
 
 # Your name may be different (store in same directory)
 FW="openwrt-ar71xx-generic-wzr-hp-g300nh2-squashfs-tftp.bin"
@@ -21,12 +22,12 @@ function setup_networking() {
 }
 
 function attempt_flash() {
-    #vecho "Plugin router and hit enter (or vice versa)"
-    # read input
+    echo "Plugin router and hit enter (or vice versa)"
+    read input
     echo "Attempting to flash firmware"
     # OR $ tftp 192.168.11.1 -c put $FW
-    curl -T $FW tftp://192.168.11.1
-    #echo "put $FW" | tftp 192.168.11.1
+    # curl -T $FW tftp://192.168.11.1
+    echo -ne "trace\ntimeout 30\nrexmt 1\nput $FW" | tftp 192.168.11.1 -v
     echo "Complete; check router LEDs"
     echo "Solid RED is bad.  Fast blink is flashing"
     echo "If good: http://wiki.openwrt.org/doc/howto/firstlogin"
