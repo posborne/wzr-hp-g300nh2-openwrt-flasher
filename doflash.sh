0a5af410bbb385b1c2542c57cded75d3132dce63
Paul Osborne <osbpau@gmail.com>
2013-09-22 17:11:39 -0500

    Doing the tftp transfer in binary appears to make a difference
    Hacks to show the images that were attempted.  The one that is currently
    flashing and appears to be working better is from the OpenWRT trunk
    (build from 22 sep 2013).  We'll see.

--- doflash.sh
+++ doflash.sh
@@ -13,7 +13,11 @@ MAC_ADDRESS="02:AA:BB:CC:DD:1A"
 # MAC_ADDRESS="00:1B:24:34:ad:b5"
 
 # Your name may be different (store in same directory)
-FW="openwrt-ar71xx-generic-wzr-hp-g300nh2-squashfs-tftp.bin"
+FW="openwrt-ar71xx-generic-wzr-hp-g300nh2-squashfs-tftp_001.bin"
+# FW="wzrhpg300nh2-pro-v24sp2-19154.enc"
+# FW="buffalo_to_ddwrt_webflash-MULTI.bin"
+# FW="openwrt-ar71xx-generic-wzr-hp-g300nh2-squashfs-tftp.bin"
+# FW="openwrt-ar71xx-generic-wzr-hp-g300nh2-jffs2-factory.bin"
 
 function setup_networking() {
     echo "Setting up network adapter"
@@ -27,7 +31,7 @@ function attempt_flash() {
     echo "Attempting to flash firmware"
     # OR $ tftp 192.168.11.1 -c put $FW
     # curl -T $FW tftp://192.168.11.1
-    echo -ne "trace\ntimeout 30\nrexmt 1\nput $FW" | tftp 192.168.11.1 -v
+    echo -ne "binary\ntrace\ntimeout 30\nrexmt 1\nput $FW" | tftp 192.168.11.1 -v
     echo "Complete; check router LEDs"
     echo "Solid RED is bad.  Fast blink is flashing"
     echo "If good: http://wiki.openwrt.org/doc/howto/firstlogin"
